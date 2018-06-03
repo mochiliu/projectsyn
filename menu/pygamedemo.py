e90f49934bd10ea399eb518e3686eb8e30cf2455
Mochi Liu <youxi3013@126.com>
2018-06-03 14:24:50 -0400

    updated test code

--- menu/pygamedemo.py
+++ menu/pygamedemo.py
@@ -6,7 +6,6 @@ pygame.init()
 
 # set up the window
 windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
 
 # set up the colors
 BLACK = (0, 0, 0)
@@ -27,27 +26,27 @@ textRect.centery = windowSurface.get_rect().centery
 # draw the white background onto the surface
 windowSurface.fill(WHITE)
 
-# draw a green polygon onto the surface
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+# # draw a green polygon onto the surface
+# pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
-# draw some blue lines onto the surface
-pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+# # draw some blue lines onto the surface
+# pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
+# pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
+# pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
 
-# draw a blue circle onto the surface
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
+# # draw a blue circle onto the surface
+# pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
 
-# draw a red ellipse onto the surface
-pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+# # draw a red ellipse onto the surface
+# pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 
 # draw the text's background rectangle onto the surface
 pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
 
-# get a pixel array of the surface
-pixArray = pygame.PixelArray(windowSurface)
-pixArray[480][380] = BLACK
-del pixArray
+# # get a pixel array of the surface
+# pixArray = pygame.PixelArray(windowSurface)
+# pixArray[480][380] = BLACK
+# del pixArray
 
 # draw the text onto the surface
 windowSurface.blit(text, textRect)
