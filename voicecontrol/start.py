68352dd5409379a297835ff56c9fe2a07b0fa9e6
Mochi Liu <youxi3013@126.com>
2018-08-25 17:56:10 -0400

    start modified to no global variables

--- voicecontrol/start.py
+++ voicecontrol/start.py
@@ -5,42 +5,39 @@ from display import LEDdisplay
 import time
 
 
-powerstate = False
-disp = LEDdisplay()
-
-def set_power_state():
+def set_power_state(powerstate):
 	# set the power state of the light panel, to save energy and reduce excess noise
 	GPIO.setmode(GPIO.BCM)
 	powersupplypin = 2
 	GPIO.setup(powersupplypin,GPIO.OUT)
 	GPIO.output(powersupplypin,powerstate)
 
-def listen_for_voice_command():
-	#listen for a command after booting up the LED strips
-	display_listening_indicator()
-	time.sleep(10)
-	powerstate = False
-	set_power_state()
 
 
-def display_listening_indicator():
+def display_listening_indicator(powerstate, disp):
 	#display on the LED panel the listening indicator
 	if not powerstate:
 		#turn the LED panel ON if it is not already on
 	    	powerstate = True
-    		set_power_state() 
+    		set_power_state(powerstate) 
 	disp.set_from_image_path("listening.bmp")
-
+	return powerstate
 
 
 if __name__ == "__main__":
+	powerstate = False
+	disp = LEDdisplay()
 
     while True:
     	# never stop
-	tt = TapTester()
+		tt = TapTester()
+		
     	while tt.doubleTap == False:
         	tt.listen()
+
         # we got a double clap!
-        listen_for_voice_command()
-        
+    	powerstate = display_listening_indicator(powerstate, disp)
+		time.sleep(10)
+		powerstate = False
+		set_power_state(powerstate)
 
