c2f6b580b839e33c7b69335cb7a1e0c185a7decb
Mochi Liu <youxi3013@126.com>
2018-10-07 19:07:02 -0400

    start changed to include lightpanel code

--- start.py
+++ start.py
@@ -1,11 +1,11 @@
-#from display import LEDdisplay
-
-#from voicecontrol import VoiceController
+import numpy as np
+from display import LEDdisplay
+from voicecontrol import VoiceController
 from ColorPDFLearner import ColorPDFLearner
 
 if __name__ == "__main__":
     powerstate = False
-    #vc = VoiceController()
+    vc = VoiceController()
     color_learner = ColorPDFLearner()
     #while True:
     # never stop
@@ -13,4 +13,8 @@ if __name__ == "__main__":
     word = input("What color?")
     ml_color = color_learner.maxlikelihoodcolor(word)
     print(ml_color)
+    if not powerstate:
+        powerstate = vc.set_LED_power_state(True)
         
+    single_color_linear_array = np.tile(ml_color, 900)
+    display.set_from_array(single_color_linear_array)
